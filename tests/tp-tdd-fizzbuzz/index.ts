21a4d46dfa3bbe39f5776176128e1d3a7771e054
Rael Calitro <rael06@hotmail.fr>
2026-06-25 10:05:44 +0200

    feat: implement calcFizzbuzz function with corresponding tests for FizzBuzz logic

---
+++ tests/tp-tdd-fizzbuzz/index.ts
@@ -0,0 +1,15 @@
+export function calcFizzbuzz(n: number): string {
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "FizzBuzz";
+  }
+
+  if (n % 3 === 0) {
+    return "Fizz";
+  }
+
+  if (n % 5 === 0) {
+    return "Buzz";
+  }
+
+  return n.toString();
+}
