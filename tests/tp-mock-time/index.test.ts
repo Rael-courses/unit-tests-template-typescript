1922b0c69f1c07d6bb0cfd0b09871550e4528c3f
Rael Calitro <rael06@hotmail.fr>
2026-06-26 14:00:40 +0200

    test(TimeUtils): set system time for isFuture test case

--- tests/tp-mock-time/index.test.ts
+++ tests/tp-mock-time/index.test.ts
@@ -4,6 +4,8 @@ describe("TimeUtils", () => {
   describe("isFuture", () => {
     it("should return true for a future date", () => {
       // Arrange
+      const fakeTimers = jest.useFakeTimers();
+      fakeTimers.setSystemTime(new Date("2026-06-26T10:13:00.000Z"));
       const futureDate = new Date("2026-06-26T10:14:00.000Z");
 
       // Act
